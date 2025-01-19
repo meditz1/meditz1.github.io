1549282972c2ea0aef6fae2b89a19fef5785fdae
Meditz <166351476+meditz1@users.noreply.github.com>
2025-01-19 18:53:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,17 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-# Initialize the count variable
 count = 0
 
 @app.route('/')
 def index():
-    return render_template('index.html', count=count)
+    return render_template('index.html')
 
-@app.route('/click', methods=['POST'])
-def click():
+@app.route('/increment', methods=['POST'])
+def increment():
     global count
-    count += 5  # Increment by 5 when the image is clicked
-    return jsonify({'count': count})
+    count += 5
+    return jsonify({"count": count})
 
 if __name__ == '__main__':
     app.run(debug=True)
