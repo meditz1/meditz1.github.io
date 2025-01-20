8e65ee6a94b77e8009b271574f74708c4f4ec701
Meditz <166351476+meditz1@users.noreply.github.com>
2025-01-19 19:40:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,1 @@
-from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)
-
-count = 0
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/increment', methods=['POST'])
-def increment():
-    global count
-    count += 5
-    return jsonify({"count": count})
-
-if __name__ == '__main__':
-    app.run(debug=True)
